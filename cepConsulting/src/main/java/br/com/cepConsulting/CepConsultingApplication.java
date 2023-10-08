51d493efd4f453d34539af8d5fdc0bf9bc53a0f5
devpolatto <polatto.xcode.job@gmail.com>
2023-10-08 17:17:11 -0300

    first commit

---
+++ cepConsulting/src/main/java/br/com/cepConsulting/CepConsultingApplication.java
@@ -0,0 +1,13 @@
+package br.com.cepConsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepConsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepConsultingApplication.class, args);
+	}
+
+}
---
+++ cepConsulting/src/main/java/br/com/cepConsulting/controller/CepController.java
@@ -0,0 +1,17 @@
+package br.com.cepConsulting.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cep")
+
+public class CepController {
+     @GetMapping("/consulta-cep")
+     public String consultaCep(){
+          return "Cep Angelo";
+     }
+     
+}
